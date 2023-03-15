85c6412d595eb7c7ba504acc29bf4211ab1b96d2
Ravi0Dubey <ravi0dubey@gmail.com>
2023-03-15 19:15:25 -0400

    changes in mongodbconnection

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,12 +1,29 @@
+
 import pymongo
 from sensor.constant.database import DATABASE_NAME
 import certifi
 ca = certifi.where()
-print("inside mongo_db connection")
+import os
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://ravi0dubey:Logiw@cluster0.0zxmnkl.mongodb.net/?retryWrites=true&w=majority"
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
+
+
+        
 
 # class MongoDBClient:
 #     client = None
-#     def _init_(self,database_name = DATABASE_NAME) -> None:
+#     def __init__(self,database_name = DATABASE_NAME) -> None:
 #         try:
 #             if MongoDBClient.client is None:
 #                 print("inside if")
@@ -19,16 +36,3 @@ print("inside mongo_db connection")
 #             self.database = self.client[database_name]
 #         except Exception as e:
 #             raise e
-
-
-class MongoDBClient:
-    client = None
-    def _init_(self,database_name = DATABASE_NAME) -> None:
-        try:
-            if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://ravi0dubey:Logiw@cluster0.0zxmnkl.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile= ca)
-            self.client = MongoDBClient.client
-            self.database = self.client[database_name]
-        except Exception as e:
-            raise e
